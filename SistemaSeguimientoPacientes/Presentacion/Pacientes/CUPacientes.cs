9ab9f47c8544ac6263a7bd1e49ffc0434cb34935
Juan <juanraar20@gmail.com>
2025-01-10 17:37:40 -0500

    OK

---
+++ SistemaSeguimientoPacientes/Presentacion/Pacientes/CUPacientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaSeguimientoPacientes.Presentacion.Pacientes
+{
+    public partial class CUPacientes : UserControl
+    {
+        public CUPacientes()
+        {
+            InitializeComponent();
+        }
+    }
+}
